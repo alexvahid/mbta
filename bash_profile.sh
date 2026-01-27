63620ffa27bcf2cf42228b253eabe900cfad8702
alexvahid <avahid@bu.edu>
2026-01-27 08:05:38 -0500

    adding bash_profile

---
+++ bash_profile.sh
@@ -0,0 +1,12 @@
+source busenv/bin/activate
+cd /home/pi/mbta
+git pull
+cp ./bash_profile ~/.bash_profile
+if [[ "$HOSTNAME" == "zero" ]]; then
+    python3 /home/pi/mbta/mbta_93.py &
+else
+    python3 /home/pi/mbta/mbta_gl.py &
+fi
+pi(){
+  python3 mbta_93.py
+}
